4e3cfbb267f728e477fe30574a53fd3463f53fd6
Tse Hiu Ming <tsehiuming@gmail.com>
2020-08-17 13:11:29 +0800

    gitpkg

---
+++ lib/typescript/src/__tests__/__fixtures__/MockRouter.d.ts
@@ -0,0 +1,22 @@
+import { Router, CommonNavigationAction, DefaultRouterOptions } from '@react-navigation/routers';
+export declare type MockActions = CommonNavigationAction | {
+    type: 'NOOP' | 'UPDATE';
+};
+export declare const MockRouterKey: {
+    current: number;
+};
+export default function MockRouter(options: DefaultRouterOptions): Router<Readonly<{
+    key: string;
+    index: number;
+    routeNames: string[];
+    history?: unknown[] | undefined;
+    routes: (Readonly<{
+        key: string;
+        name: string;
+        params?: object | undefined;
+    }> & {
+        state?: Readonly<any> | import("@react-navigation/routers").PartialState<Readonly<any>> | undefined;
+    })[];
+    type: string;
+    stale: false;
+}>, MockActions>;
